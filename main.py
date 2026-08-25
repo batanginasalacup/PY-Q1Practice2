e34099b40a70918b6487288c2b2c0c9c44b7d807
angel baby <salacup.jm@obmontessori.edu.ph>
2026-08-25 08:49:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ y = 2025 #integer
 z = 3.14
 a = True #boolean
 b = ['mama', 'mo', 'blue'] #list
+c = (6,7,11) #tuple
+d = {6,7,11} #set
+e = {'sapphire', 'sapphire', 'sapphire'} #set with string
 
 display('The data type of x is', type(x), target="div1") #display output in div
 display(type(y), target="div1") 
 display(type(z), target="div1")
 display(type(a), target="div1")
+display(type(b), target="div1")
+display(type(b), target="div1")
 

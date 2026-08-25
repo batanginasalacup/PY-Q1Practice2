fb923830b126d6235ea535dacc135abf269a7fc7
angel baby <salacup.jm@obmontessori.edu.ph>
2026-08-25 08:43:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from pyscript import display
 x = "Year" #string
 y = 2025 #integer
 z = 3.14
+a = True #boolean
+b = ['mama', 'mo', 'blue'] #list
 
 display('The data type of x is', type(x), target="div1") #display output in div
 display(type(y), target="div1") 
 display(type(z), target="div1")
+display(type(a), target="div1")
 

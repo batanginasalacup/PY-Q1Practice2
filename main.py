4884d751b6e6fce5e390d62e4d53b27d285b5e89
angel baby <salacup.jm@obmontessori.edu.ph>
2026-09-08 07:43:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,6 @@
-from pyscript import display
+#integer
 
-x = "Year" #string
-y = 2025 #integer
-z = 3.14
-a = True #boolean
-b = ['mama', 'mo', 'blue'] #list
-c = (6,7,11) #tuple
-d = {6,7,11} #set
-e = {'sapphire', 'sapphire', 'sapphire'} #set with string
-f = {
-    "name" : "Mateo",
-    "age" : 15,
-    "description" : "fat"
-}
+#0
 
-display('The data type of x is', type(x), target="div1") #display output in div
-display(type(y), target="div1") 
-display(type(z), target="div1")
-display(type(a), target="div1")
-display(type(b), target="div1")
-display(type(c), target="div1")
-display(type(d), target="div1")
-display(type(e), target="div1")
-display(type(f), target="div1")
-display(f["name"], f["description"])
- 
+number = 03
+display(number)
